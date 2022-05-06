049b7885a410ca56ee5bace6b2f013e4be7d73dc
KnightNight <gilbenvenitz18@gmail.com>
2022-05-06 16:08:43 -0500

    everything works except category Delete route

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,5 @@
+-- FOR the sake of this video just know I named my Database = "ecommerce_db"
+
 -- DROP DATABASE
 DROP DATABASE IF EXISTS ecommerce_db;
 
--- models/index.js
+++ models/index.js
@@ -7,28 +7,36 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
+  as: 'Associated Categories',
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE',
+  as: 'Associated Products',
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
   through: {
     model: ProductTag,
     unique: false,
   },
+  as: 'tags',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE',
   through: {
     model: ProductTag,
     unique: false,
   },
+  as: 'products',
 });
 
 module.exports = {
